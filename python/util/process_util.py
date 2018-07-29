b2dfb626949cebb669fdbb3dfafed08063593a73
bgun26 <bgun26@users.noreply.github.com>
2018-07-29 14:27:20 +0200

    Changed some method signatures and updated documentation

--- python/util/process_util.py
+++ python/util/process_util.py
@@ -14,19 +14,32 @@ class ProcessExecutor:
         storing and/or filtering the output.
     """
 
-    def __init__(self, command, shell=True, cwd=None, logger=logging.debug):
+    def __init__(self, command, shell=True, cwd=None):
         """
         Constructor initializing instance variables.
-            command: is either a string or a list of strings
-            shell:   boolean indicating if the command should be executed through the shell
-            cwd:     if not None, the command is executed in the directory given by cwd
-            logger:  logger object
+
+        Args:
+            command (str or List): is either a string or a list of strings,
+
+        Keyword Args:
+            shell (bool): indicating if the command should be executed through the shell.
+                without the shell, environment variables like ~ and $EDITOR are not expanded,
+                aliases are not effective etc.
+            cwd (str): the command is executed in the directory given by cwd,
+                if None, the command is executed in the current directory
+            logger (Callable): logger object, use None to disable logging
+
+        Attributes:
+            All private attributes are wrapped by properties and documented there.
+
+        Create an instance by specifying the execution options of the command.
+
+        >>> p1 = ProcessExecutor(['ls', '-l', '~/Documents'])
+        >>> p2 = ProcessExecutor('pwd', cwd='..')
         """
         self._command = ' '.join(command) if isinstance(command, list) else str(command)
         self._use_shell = shell
         self._cwd = cwd
-        # if logger is given as None, use an empty function as logger
-        self._logger = logger or (lambda *x: None)
         self._return_code = None
         self._output_log = None
 
@@ -49,11 +62,6 @@ class ProcessExecutor:
         return self._cwd
 
     @property
-    def logger(self):
-        """ The logger object used by the instance """
-        return self._logger
-
-    @property
     def return_code(self):
         """ Return code of the executed command, None if the command is not yet executed """
         return self._return_code
@@ -67,40 +75,71 @@ class ProcessExecutor:
         return self._output_log
 
     # Private methods
-    def _gen_execute_process(self):
+    def _gen_execute_process(self, logger_obj=logging.debug):
         """
-        Returns a generator that produces the output of the executed command line by line.
-        Stores the return code of the command in the instance variable.
+        Executes the command parameterized by the instance variables,
+        stores the exit code into an instance variable.
+        Logs the output line by line.
+
+        Yields:
+            A generator that produces the output line by line
+
+        The process executing the command is run uninterrupted from start to finish.
+        The generator instead of a function only makes a difference about how the
+        output of the command is stored. Generator is more memory efficient than
+        storing the entire output as a list.
+
+        >>> for line in self._gen_execute_process():
+        >>>     print("Processing line: '%s'" % line)
         """
-        self._logger("Executing process '%s'", self._command)
+
+        logger_obj("Executing process '%s'", self._command)
         p = subprocess.Popen(self._command, shell=self._use_shell, cwd=self._cwd,
-                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                             encoding='utf-8')
         while True:
-            out_line = p.stdout.readline().rstrip().decode('utf-8')
+            out_line = p.stdout.readline().rstrip()
             if p.poll() is not None and out_line == "":
                 self._return_code = p.returncode
                 break
             if out_line:
-                self._logger(out_line)
+                logger_obj(out_line)
                 yield out_line
 
     # Public methods
-    def execute(self, match_regex=None, regex_match_handler=None, store_output_log=False):
+    def execute(self, logger=logging.debug, store_output=False,
+                match_regex=None, regex_match_handler=None):
         """
-        Executes the command, returns return value of the command.
+        Executes the command, optionally stores the output log
+        and/or redirects matching output lines to a handler.
 
-        store_output_log:    if True, stores the output log in an instance variable.
-        match_regex:         each line is searched with this pattern,
-                             matches are passed to regex_match_handler as a match object.
-                             if None, every line is a match and passed as a string
-        regex_match_handler: callable which accepts one argument
+        Keyword Args:
+            logger (Callable): this callable is used to log the execution
+            store_output (bool): if True, stores the output log in an instance variable
+            match_regex (str): each line is searched with this regex pattern,
+                matches are passed to regex_match_handler as a re.match object.
+                if None, every line is a match and passed as a string
+            regex_match_handler (Callable): callable object which accepts one argument
+
+        Returns:
+            The exit code of the executed process
+
+        It is used after initializing a ProcessExecutor object.
+
+        >>> p = ProcessExecutor('seq 1 3')
+        >>> return_code = p.execute(store_output_log=True)
+        >>> assert return_code == p.return_code
+        >>> output_log = p.output_log
         """
+
+        # if logger is given as None, an empty function is used as logger
+        logger_obj = logger or (lambda *x: None)
         match_handler = regex_match_handler or (lambda match: None)
         compiled_regex = re.compile(match_regex) if match_regex else None
-        self._output_log = [] if store_output_log else None
+        self._output_log = [] if store_output else None
 
-        for output_line in self._gen_execute_process():
-            if store_output_log:
+        for output_line in self._gen_execute_process(logger_obj=logger_obj):
+            if store_output:
                 self._output_log.append(output_line)
             if compiled_regex:
                 match = compiled_regex.match(output_line)
@@ -112,19 +151,29 @@ class ProcessExecutor:
         return self._return_code
 
     def execute_check_return(self, expected_return_code=os.EX_OK,
-                             match_regex=None, regex_match_handler=None,
-                             store_output_log=False):
+                             logger=logging.debug, store_output=False,
+                             match_regex=None, regex_match_handler=None):
         """
-        Convenience wrapper around execute method,
-        returns True if the return code of the command is expected_return_code,
-        returns False otherwise.
+        Convenience wrapper around ProcessExecutor.execute method.
 
-        For other options, see execute method
+        Keyword Args:
+            expected_return_code (int): compared to the exit code of the executed command
+
+        Returns:
+            True if the return code of the command is expected_return_code
+            False otherwise
+
+        Can be used to quickly check a resource, connection etc.
+
+        >>> HOST_NAME = 'google.com'
+        >>> p = ProcessExecutor(['ping', '-q', '-c', '1', HOST_NAME])
+        >>> if p.execute_check_return(): print("%s is accessible" % HOST_NAME)
         """
         result = False
-        return_code = self.execute(match_regex=match_regex,
-                                   regex_match_handler=regex_match_handler,
-                                   store_output_log=store_output_log)
+        return_code = self.execute(logger=logger,
+                                   store_output=store_output,
+                                   match_regex=match_regex,
+                                   regex_match_handler=regex_match_handler)
         if return_code == expected_return_code:
             result = True
         return result
@@ -134,6 +183,8 @@ if __name__ == '__main__':
 
     def line_handler(line):
         logging.info("Processing output line: '%s' of type %s", line, type(line))
+        # import time
+        # time.sleep(1)
 
     def regex_handler(match):
         logging.info("Processing regex match: '%s' of type %s",
@@ -146,8 +197,8 @@ if __name__ == '__main__':
     p.execute(regex_match_handler=line_handler)
     # with different output handling options
     p.execute(match_regex=r"^2", regex_match_handler=regex_handler)
-    p.execute(store_output_log=True)
-    logging.info("Stored output log: '%s'", p._output_log)
+    p.execute(store_output=True, logger=None)
+    logging.info("Stored output: '%s'", p._output_log)
 
     HOST_NAME = 'google.com'
     p2 = ProcessExecutor(['ping', '-q', '-c', '1', HOST_NAME])
